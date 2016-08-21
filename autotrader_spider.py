d15f10976cf83b2a44f046b948bb7b47dd1c317a
Kayra <KayraAlat@gmail.com>
2016-08-21 17:21:32 +0100

    Added stats dict

--- autotrader_spider.py
+++ autotrader_spider.py
@@ -22,7 +22,16 @@ class AutoTraderSpider(scrapy.Spider):
         prices = response.xpath('//article/div/div[2]/div[1]/div/text()').extract()
         links = response.xpath('//article/div/div[2]/div[1]/h1/a/@href').extract()
 
-        for title, stat, price, link in zip(titles, stats, prices, links):
-            stat = stat.xpath('li/text()').extract()
+        for title, car_stats, price, link in zip(titles, stats, prices, links):
+            car_stats = car_stats.xpath('li/text()').extract()
+
+            stats_dict = dict()
+            stats_dict['year'] = car_stats[0]
+            stats_dict['style'] = car_stats[1]
+            stats_dict['milage'] = car_stats[2]
+            stats_dict['transmission'] = car_stats[3]
+            stats_dict['size'] = car_stats[4]
+            stats_dict['fuel'] = car_stats[5]
+
             price = price.replace('£', '').replace(',', '')
-            yield {'car': (title, stat, price, link)}
+            yield {'car': (title, stats_dict, price, link)}
